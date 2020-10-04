fe536f3621e58949a71ae81e629a0eee88fc4776
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-10-04 14:34:27 -0500

    Module RFX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavbarMainComponent }          from './components/adminlte/navbar/navbar-main/navbar-main.component';
+import { NavbarLeftLinksComponent }     from './components/adminlte/navbar/navbar-left-links/navbar-left-links.component';
+import { NavbarSearchFormComponent }    from './components/adminlte/navbar/navbar-search-form/navbar-search-form.component';
+import { NavbarRightLinksComponent }    from './components/adminlte/navbar/navbar-right-links/navbar-right-links.component';
+import { SidebarMainComponent }         from './components/adminlte/sidebar/sidebar-main/sidebar-main.component';
+import { SidebarBrandLogoComponent }    from './components/adminlte/sidebar/sidebar-brand-logo/sidebar-brand-logo.component';
+import { SidebarMenuComponent }         from './components/adminlte/sidebar/sidebar-menu/sidebar-menu.component';
+import { SidebarMenuUserinfoComponent } from './components/adminlte/sidebar/sidebar-menu-userinfo/sidebar-menu-userinfo.component';
+import { FooterMainComponent }          from './components/adminlte/footer/footer-main/footer-main.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarMainComponent,
+    NavbarLeftLinksComponent,
+    NavbarSearchFormComponent,
+    NavbarRightLinksComponent,
+    SidebarMainComponent,
+    SidebarBrandLogoComponent,
+    SidebarMenuComponent,
+    SidebarMenuUserinfoComponent,
+    FooterMainComponent
   ],
   imports: [
     BrowserModule,
